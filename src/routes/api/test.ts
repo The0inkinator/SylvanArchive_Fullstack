1efcc08f823b9c9fa5585e7dfdbd005679e55565
TheOinkinator <theoinkinator@gmail.com>
2023-10-08 22:55:57 -0700

    remove cors

--- src/routes/api/test.ts
+++ src/routes/api/test.ts
@@ -1,10 +1,8 @@
 import { request } from "http";
 import { MongoClient } from "mongodb";
 import { json } from "solid-start/server";
-const cors = require("cors");
 
 export async function GET() {
-  const corsMiddleWare = cors();
   const uri =
     "mongodb+srv://SylvanArchiveAPI:getAPIPass@sylvanarchivedb.zodmskg.mongodb.net/";
   const client = new MongoClient(uri);
@@ -20,9 +18,8 @@ export async function GET() {
       await client.close();
       console.log("Connection closed");
       console.log("returning data");
-      corsMiddleWare(request, () => {
-        resolve(json(bindersData));
-      });
+
+      resolve(json(bindersData));
     } catch (err) {
       console.error("Error connecting to database", err);
       const errorData = { error: "error" };
