c819c374cb91d7ba1552762ce542bef82b3fa189
TheOinkinator <theoinkinator@gmail.com>
2023-10-09 12:17:37 -0700

    connection test for api

--- src/routes/api/test.ts
+++ src/routes/api/test.ts
@@ -3,32 +3,35 @@ import { json } from "solid-start/server";
 import fetch from "../../backend/fetch";
 
 export async function GET() {
-  // const uri =
-  //   "mongodb+srv://SylvanArchiveAPI:getAPIPass@sylvanarchivedb.zodmskg.mongodb.net/";
-  // const client = new MongoClient(uri);
+  const uri =
+    "mongodb+srv://SylvanArchiveAPI:getAPIPass@sylvanarchivedb.zodmskg.mongodb.net/";
+  const client = new MongoClient(uri);
 
-  // return new Promise(async (resolve, reject) => {
-  //   try {
-  //     await client.connect();
-  //     const db = client.db("sylvanArchiveDB");
-  //     console.log("connected");
-  //     const binders = db.collection("binders");
-  //     const cursor = binders.find({});
-  //     const bindersData = await cursor.toArray();
-  //     await client.close();
-  //     console.log("Connection closed");
-  //     console.log("returning data");
+  let connectionStatus = false;
 
-  //     resolve(json(bindersData));
-  //   } catch (err) {
-  //     console.error("Error connecting to database", err);
-  //     const errorData = { error: "error" };
-  //     const data = JSON.stringify(errorData);
-  //     resolve(json({ error: "No Data" }));
-  //   }
-  // });
+  try {
+    await client.connect();
+    const db = client.db("sylvanArchiveDB");
+    console.log("connected");
+    connectionStatus = true;
+    // const binders = db.collection("binders");
+    // const cursor = binders.find({});
+    // const bindersData = await cursor.toArray();
+    // await client.close();
+    // console.log("Connection closed");
+    // console.log("returning data");
 
-  const data = await fetch();
+    // resolve(json(bindersData));
+  } catch (err) {
+    console.error("Error connecting to database", err);
+    const errorData = { error: "error" };
+    const data = JSON.stringify(errorData);
+    // resolve(json({ error: "No Data" }));
+  }
+
+  // const data = await fetch();
+
+  const data = { Status: connectionStatus };
 
   return json(data);
 }
