5e67e0c5ba62555bdb9cfc4bb485cb533b4a08b2
TheOinkinator <theoinkinator@gmail.com>
2023-10-08 22:21:22 -0700

    update ssr

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import solid from "solid-start/vite";
 import vercel from "solid-start-vercel";
 
 export default defineConfig({
-  plugins: [solid({ adapter: vercel() })],
+  plugins: [solid({ ssr: true, adapter: vercel() })],
 });
