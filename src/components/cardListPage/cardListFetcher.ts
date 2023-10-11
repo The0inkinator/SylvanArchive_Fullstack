84f97eca72ead9d5a48b35607b3462ef4f14b885
TheOinkinator <theoinkinator@gmail.com>
2023-10-11 14:17:40 -0700

    update to fetch from mongo db across the board

--- src/components/cardListPage/cardListFetcher.ts
+++ src/components/cardListPage/cardListFetcher.ts
@@ -3,9 +3,7 @@ import { useCardListContext } from "~/context/CardListContext";
 export default async function cardListFetcher(cardListPath: string) {
   const [cardList, { makeCardList }]: any = useCardListContext();
   try {
-    const cardListData = await fetch(
-      "https://sylvan-archive-api-03b13d1a78b5.herokuapp.com/tables/cardLists"
-    );
+    const cardListData = await fetch("/api/tables/cardLists");
     const tempCardList = await cardListData.json();
 
     interface cardInfo {
