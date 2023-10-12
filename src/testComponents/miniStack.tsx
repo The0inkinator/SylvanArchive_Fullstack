abb45ef6f4c7648392081d4abb61833063015b1a
TheOinkinator <theoinkinator@gmail.com>
2023-10-12 13:51:46 -0700

    add setblue to ministack

--- src/testComponents/miniStack.tsx
+++ src/testComponents/miniStack.tsx
@@ -1,14 +1,21 @@
+import { createSignal } from "solid-js";
+
 interface stackNameInput {
   stackName: string;
 }
 
 export default function MiniStack({ stackName }: stackNameInput) {
+  const [blue, setBlue] = createSignal<boolean>(false);
+
   return (
     <div
+      onclick={() => {
+        setBlue(true);
+      }}
       style={{
         width: "2rem",
         height: "2rem",
-        "background-color": "red",
+        "background-color": blue() ? "blue" : "red",
         color: "white",
       }}
     >
