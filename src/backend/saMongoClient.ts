e0f9558c73f83d0c14e87e24c8d61d6433e5c80e
TheOinkinator <theoinkinator@gmail.com>
2023-10-11 22:41:10 -0700

    fix inccorectly closed mongo connections

--- src/backend/saMongoClient.ts
+++ src/backend/saMongoClient.ts
@@ -4,5 +4,5 @@ export function saMongoClient() {
   const uri =
     "mongodb+srv://SylvanArchiveAPI:getAPIPass@sylvanarchivedb.zodmskg.mongodb.net/";
   const client = new MongoClient(uri);
-  return client;
+  return client as any;
 }
