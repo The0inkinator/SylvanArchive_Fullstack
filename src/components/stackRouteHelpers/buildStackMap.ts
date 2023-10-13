11aebbf40e994632287b9f08785aa230403627e4
TheOinkinator <theoinkinator@gmail.com>
2023-10-13 15:35:05 -0700

    remove old build stack map

--- src/components/stackRouteHelpers/buildStackMap.ts
+++ src/components/stackRouteHelpers/buildStackMap.ts
@@ -1,19 +1,3 @@
-// import { useStackMapContext } from "../../context/StackMapContext";
-
-// export default async function buildStackMap() {
-//   const [stackMap, { makeStackMap }]: any = useStackMapContext();
-
-//   try {
-//     const binderData = await fetch("/api/tables/newBinders");
-//     const binderJson = await binderData.json();
-//     makeStackMap(binderJson);
-//     return "Built";
-//   } catch (err) {
-//     console.error("Error fetching stackmap", err);
-//     return "Failed";
-//   }
-// }
-
 import { useStackMapContext } from "../../context/StackMapContext";
 import { useStackStateContext } from "../../context/StackStateContext";
 
